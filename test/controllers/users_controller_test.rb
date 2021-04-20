e3143b56fc72ce0fe517858aceae02e4b9bdcee4
watanabe saiya <say-ya111@users.noreply.github.com>
2021-04-21 03:37:02 +0900

    テスト削除

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,17 +11,4 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get user_path(@user)
     assert_response :success
   end
-
-  # ローテーションの数値がそのままだと新たなの部位別トレーニングが途中からになる
-  test "週あたりトレーニング回数更新に合わせてローテーションをリセットする" do
-    skip
-    assert_not @user.training_rotation == 0
-    # トレーニング回数を変更
-    patch user_path(@user), params: {
-      user: {
-        times_a_week: 2
-      }
-    }, xhr: true
-    assert @user.training_rotation == 0
-  end
 end
